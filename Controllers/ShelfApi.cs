a73e5decaaec42fac71fb040c294f86fbf7d41b5
MaggieChafee <maggie.chafee@gmail.com>
2024-05-20 20:18:17 -0500

    user endpoints completed

--- Controllers/ShelfApi.cs
+++ Controllers/ShelfApi.cs
@@ -7,10 +7,21 @@ namespace Books.Controllers
 {
     public class ShelfApi
     {
+        // get a user's shelves
         public static void Map(WebApplication app)
         {
-            app.MapGet("", () => {
+            app.MapGet("/shelves/user/{userId}", (BooksDbContext db, int userId) => 
+            {
+                var userShelves = db.Shelves
+                    .Include(s => s.Books)
+                    .Where(s => s.UserId == userId)
+                    .ToList();
 
+                if (userShelves == null)
+                {
+                    return Results.Empty;
+                }
+                return Results.Ok(userShelves);
             });
         }
     }
--- Controllers/UserApi.cs
+++ Controllers/UserApi.cs
@@ -2,6 +2,7 @@
 using Books.Controllers;
 using Microsoft.AspNetCore.Mvc.Routing;
 using Microsoft.EntityFrameworkCore;
+using Books.Models;
 
 namespace Books.Controllers
 {
@@ -9,8 +10,37 @@ namespace Books.Controllers
     {
         public static void Map(WebApplication app)
         {
-            app.MapGet("", () => {
+            app.MapGet("/users/{userId}", (BooksDbContext db, int userId) =>
+            {
+                var singleUser = db.Users.FirstOrDefault(u => u.Id == userId);
 
+                if (singleUser == null)
+                {
+                    return Results.BadRequest();
+                }
+
+                return Results.Ok(singleUser);
+            });
+
+            // check to see if a user is in the system based on uid
+            app.MapGet("/checkuser/{uid}", (BooksDbContext db, string uid) =>
+            {
+                var user = db.Users.FirstOrDefault(u => u.Uid == uid);
+
+                if (user == null)
+                {
+                    return Results.NotFound();
+                }
+                return Results.Ok(user);
+            });
+
+            // create user
+            app.MapPost("/users", (BooksDbContext db, User newUser) =>
+            {
+                db.Users.Add(newUser);
+                db.SaveChanges();
+
+                return Results.Created($"/users/{newUser.Id}", newUser);
             });
         }
     }
